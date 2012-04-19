b6d304008844b4181d298f205e60963b06f6704f
Luka Woititz <luka.woititz@uni-mb.si>
2012-04-19 18:51:19 +0200

    scanner, parser in atributna gramatika v enem. "stand-alone" aplikacija

--- Leksikalni analizator/Leksikalni analizator/Form1.cs
+++ Leksikalni analizator/Leksikalni analizator/Form1.cs
@@ -12,6 +12,292 @@ namespace Leksikalni_analizator
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> pomnilnik = new Dictionary<string, string>();
+
+        string izpis = "";
+
+        public bool parse()
+        {
+            token Token = Tokenizer.nextToken();
+            return PRI() && Tokenizer.currentToken().eof;
+        }
+
+        public bool PRI()
+        {
+            if (Tokenizer.currentToken().type == "vara" || Tokenizer.currentToken().type == "varb")
+            {
+                token spremenljivka = Tokenizer.currentToken();
+                Tokenizer.nextToken();
+
+                if (Tokenizer.currentToken().type == "pri2")
+                {
+                    Tokenizer.nextToken();
+
+                    List<double> rezultati = new List<double>();
+                    
+                    bool rezultat=EP(ref rezultati);
+
+                    if (Tokenizer.currentToken().lexeme == ";")
+                    {
+                        Tokenizer.nextToken();
+
+                        string izpisRezultati = "";
+                        foreach (var element in rezultati)
+                        {
+                            izpisRezultati += element.ToString()+" ";
+                        }
+
+                        if (!pomnilnik.ContainsKey(spremenljivka.lexeme))
+                            pomnilnik.Add(spremenljivka.lexeme, izpisRezultati);
+                        else pomnilnik[spremenljivka.lexeme] = izpisRezultati;
+
+                        return rezultat && PRI();
+                    }
+
+                    else return false;
+                }
+
+                else return false;
+            }
+
+            else if(Tokenizer.currentToken().lexeme == "print")
+            {
+                Tokenizer.nextToken();
+
+                if (Tokenizer.currentToken().type == "vara" || Tokenizer.currentToken().type == "varb")
+                {
+                    token spremenljivka = Tokenizer.currentToken();
+                    Tokenizer.nextToken();
+
+                    if (Tokenizer.currentToken().lexeme == ";")
+                    {
+                        Tokenizer.nextToken();
+
+                        if (pomnilnik.ContainsKey(spremenljivka.lexeme))
+                            izpis += "Spremenljivka " + spremenljivka.lexeme + " ima vrednost " + pomnilnik[spremenljivka.lexeme] + "\r\n";
+                        else izpis += "Ne obstaja spremenljivka " + spremenljivka.lexeme + "\r\n";
+
+                        return PRI();
+                    }
+
+                    else return false;
+                }
+
+                else return false;
+            }
+
+            else return true;
+        }
+
+        public bool EP(ref List<double> polje)
+        {
+            if (Tokenizer.currentToken().lexeme == "abs" || Tokenizer.currentToken().lexeme == "cos" || Tokenizer.currentToken().lexeme == "sin" || Tokenizer.currentToken().lexeme == "sin" || Tokenizer.currentToken().lexeme == "(" || Tokenizer.currentToken().type == "float" || Tokenizer.currentToken().type == "float3" || Tokenizer.currentToken().lexeme == "-" || Tokenizer.currentToken().type == "vara" || Tokenizer.currentToken().type == "varb")
+            {
+                double vrednostIzraza = 0;
+                bool rezultatE = E(ref vrednostIzraza);
+                polje.Add(vrednostIzraza);
+                return rezultatE;
+            }
+
+            else if (Tokenizer.currentToken().lexeme == "{")
+            {
+                return POLJE(ref polje);
+            }
+
+            else return false;
+        }
+
+        public bool POLJE(ref List<double> polje)
+        {
+            if (Tokenizer.currentToken().lexeme == "{")
+            {
+                Tokenizer.nextToken();
+                bool rezultat = NUMS(ref polje);
+
+                if (Tokenizer.currentToken().lexeme == "}")
+                {
+                    Tokenizer.nextToken();
+
+                    return rezultat;
+                }
+
+                else return false;
+            }
+
+            return false;
+        }
+
+        public bool NUMS(ref List<double> polje)
+        {
+            double stevilo = 0;
+            bool rezultatF = F(ref stevilo);
+            polje.Add(stevilo);
+            bool rezultatNUM = NUM(ref polje);
+            return rezultatF && rezultatNUM;
+        }
+
+        public bool NUM(ref List<double> polje)
+        {
+            if (Tokenizer.currentToken().lexeme == ",")
+            {
+                Tokenizer.nextToken();
+
+                double stevilo = 0;
+                bool rezultatF = F(ref stevilo);
+                polje.Add(stevilo);
+                bool rezultatNUM = NUM(ref polje);
+                return rezultatF && rezultatNUM;
+            }
+
+            return true;
+        }
+
+        bool E(ref double stevilo)
+        {
+            bool rezultatT = T(ref stevilo);
+            bool rezultatEE = EE(ref stevilo);
+            return rezultatT && rezultatEE;
+        }
+
+        bool EE(ref double vhodnoStevilo)
+        {
+            if (Tokenizer.currentToken().lexeme == "+" ||
+                Tokenizer.currentToken().lexeme == "-")
+	        {
+                token spremenljivka = Tokenizer.currentToken();
+                Tokenizer.nextToken();
+                double stevilo = 0;
+
+                bool rezultatT = T(ref stevilo);
+
+                bool rezultatEE = EE(ref vhodnoStevilo);
+
+                if (spremenljivka.lexeme == "+")
+                    vhodnoStevilo = stevilo + vhodnoStevilo;
+                else
+                    vhodnoStevilo = stevilo - vhodnoStevilo;
+
+                return rezultatT && rezultatEE;
+	        } 
+            else return true;
+        }
+
+        bool T(ref double stevilo)
+        {
+            bool rezultatF= F(ref stevilo);
+            bool rezultatTT=TT(ref stevilo);
+            return rezultatF && rezultatTT;
+        }
+
+        bool TT(ref double vhodnoStevilo)
+        {
+            if (Tokenizer.currentToken().lexeme == "/"
+                || Tokenizer.currentToken().lexeme == "*")
+            {
+                token spremenljivka = Tokenizer.currentToken();
+                Tokenizer.nextToken();
+
+                double stevilo = 0;
+                bool rezultatF = F(ref stevilo);
+                bool rezultatTT = TT(ref vhodnoStevilo);
+
+                if (spremenljivka.lexeme == "/")
+                    vhodnoStevilo = stevilo / vhodnoStevilo;
+                else vhodnoStevilo = stevilo * vhodnoStevilo;
+
+                return rezultatF && rezultatTT;
+            }
+            else return true;
+        }
+
+        bool F(ref double vhodnoStevilo)
+        {
+            if (Tokenizer.currentToken().type == "abs3"
+                || Tokenizer.currentToken().type == "sin3"
+                || Tokenizer.currentToken().type == "cos3")
+            {
+                token spremenljivka = Tokenizer.currentToken();
+                Tokenizer.nextToken();
+                if (Tokenizer.currentToken().lexeme == "(")
+                {
+                    Tokenizer.nextToken();
+                    bool temp = E(ref vhodnoStevilo);
+                    switch (spremenljivka.lexeme)
+                    {
+                        case "abs":
+                            vhodnoStevilo = Math.Abs(vhodnoStevilo);
+                            break;
+                        case "sin":
+                            vhodnoStevilo = Math.Sin(vhodnoStevilo);
+                            break;
+                        case "cos":
+                            vhodnoStevilo = Math.Cos(vhodnoStevilo);
+                            break;
+                    }
+                    if (Tokenizer.currentToken().lexeme == ")")
+                    {
+                        Tokenizer.nextToken();
+                        return temp;
+                    }
+                    else return false;
+                }
+                return false;
+            }
+            else if (Tokenizer.currentToken().lexeme == "(")
+            {
+                Tokenizer.nextToken();
+                bool temp = E(ref vhodnoStevilo);
+
+                if (Tokenizer.currentToken().lexeme == ")")
+                {
+                    Tokenizer.nextToken();
+                    return temp;
+                }
+                else return false;
+            }
+            else if (Tokenizer.currentToken().type == "float"
+                || Tokenizer.currentToken().type == "float3"
+                )
+            {
+                vhodnoStevilo = double.Parse(Tokenizer.currentToken().lexeme);
+
+                Tokenizer.nextToken();
+                return true;
+            }
+
+            else if (Tokenizer.currentToken().lexeme == "-")
+            {
+                Tokenizer.nextToken();
+                if (Tokenizer.currentToken().type == "float"
+                || Tokenizer.currentToken().type == "float3"
+                )
+                {
+                    vhodnoStevilo = double.Parse(Tokenizer.currentToken().lexeme);
+
+                    Tokenizer.nextToken();
+                    return true;
+                }
+                else return false;
+            }
+            else if (Tokenizer.currentToken().type == "vara"
+                || Tokenizer.currentToken().type == "varb")
+            {
+                try
+                {
+                    if (pomnilnik.ContainsKey(Tokenizer.currentToken().lexeme))
+                        vhodnoStevilo = double.Parse(pomnilnik[Tokenizer.currentToken().lexeme]);
+                }
+                catch (Exception ex)
+                {
+                    izpis += "Ne da se uporabiti spremenljivke z polju v aritmetičnih izrazih!\r\n";
+                    vhodnoStevilo = double.NaN;
+                }
+                Tokenizer.nextToken();
+                return true;
+            }
+            return false;
+        }
+
         class tokenizer
         {
             List<Char> abeceda;
@@ -25,6 +311,12 @@ namespace Leksikalni_analizator
             int row = 1, startRow=1;
             int pos = 0;
             bool stop = false;
+            token current;
+
+            public token currentToken()
+            {
+                return current;
+            }
 
             public tokenizer(Dictionary<string, List<string>> pravila, List<char> abeceda, List<string> končna_stanja, String filename)
             {
@@ -41,11 +333,11 @@ namespace Leksikalni_analizator
 
             public token nextToken()
             {
-                if (stop) return new token(-1, -1, true, "eof", "eof");
+                if (stop) return current = new token(-1, -1, true, "eof", "eof");
                 if (pos == file.Length)
                 {
                     stop = true;
-                    return new token(startCol, startRow, true, lexeme, stanje);
+                    return current = new token(startCol, startRow, true, lexeme, stanje);
                 }
                 if (file[pos] == '\n')
                 {
@@ -72,6 +364,7 @@ namespace Leksikalni_analizator
                 if ((produkcija == "~" || pos == file.Length) && končna.IndexOf(stanje) != -1)
                 {//produkcije ni v tabeli, stanje je pa končno, vrnemo token.
 
+
                     String oldLexeme = lexeme;
                     String oldStanje = stanje;
                     stanje = "start";
@@ -79,8 +372,11 @@ namespace Leksikalni_analizator
                     int oldCol = startCol, oldRow = startRow;
                     startCol = col;
                     startRow = row;
+                    //int oldPos = pos;
+                    //pos++;
+                    //col++;
 
-                    return new token(oldCol, oldRow, pos == file.Length, oldLexeme, oldStanje);
+                    return current = new token(oldCol, oldRow, false, oldLexeme, oldStanje);
                 }
 
                 //produkcija je veljavna
@@ -166,6 +462,12 @@ namespace Leksikalni_analizator
                 temp = Tokenizer.nextToken();
                 textBox1.Text += "Vrednost: " + temp.lexeme + "\tStanje: " + temp.type + "\tCol: " + temp.col + "\tRow:" + temp.row + "\tEof:" + temp.eof + "\r\n";
             } while (!temp.eof);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool parsed = parse();
+            textBox1.Text += "Parse uspel: " + parsed + "\r\nIzpis:\r\n"+izpis;
         }
     }
 }
